0a6bf984825fffa3474d0dcbcef2efd6c4a54389
Nathan Pare <nathanpare@Nathans-MBP.lan>
2022-07-28 13:05:31 -0700

    Home Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import Sidebar from './views/sidebar/Sidebar';
 import LibraryPage from './views/Library/library';
 //import {BrowserRouter as Router, Switch, Route} from 'react-dom';
 
---
+++ frontend/src/views/Nav/Nav.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <div className='nav'>
+      <nav>
+        <div className='logo'>
+    <p>Reverb</p>
+    </div>
+    <div className="right">
+      <div className="login">
+        <p>Login</p>
+      </div>
+      <div className="account">
+        <p>Account</p>
+      </div>
+      <div className="about">
+        <p>About</p>
+      </div>
+    </div>
+  </nav>
+    </div>
+  )
+}
