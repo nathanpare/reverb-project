7c0b64f13255fd2abd7dc1206eea2ed71d8873bd
Robert812218 <robert812@shaw.ca>
2022-07-28 13:55:43 -0600

    full send

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Sidebar from './views/sidebar/Sidebar';
+import LibraryPage from './views/Library/library';
 //import {BrowserRouter as Router, Switch, Route} from 'react-dom';
 
 
 function App() {
   return (
   <div className='App'>
-  <Sidebar />
+    <LibraryPage />
   </div>
 
   );
---
+++ frontend/src/views/Library/library.js
@@ -0,0 +1,318 @@
+
+
+// import './App.css';
+
+
+
+
+
+
+
+// function App() {
+
+
+//   return (
+
+
+//     <div className="App">
+
+
+//       <header className="App-header">
+
+
+//       </header>
+
+
+  
+
+
+//     </div>
+
+
+//   );
+
+
+// }
+
+
+
+
+
+
+// export default App;
+
+
+
+
+
+
+
+const LibraryPage = () => {
+
+
+  return (
+ 
+ 
+    <div className="Page-content">
+ 
+ 
+      <div className="Page-top">
+ 
+ 
+        <Header />
+ 
+ 
+      </div>
+ 
+ 
+      <div className="Page-center">
+ 
+ 
+        <LeftBar />
+ 
+ 
+        <Body />
+ 
+ 
+        <RightBar />
+ 
+ 
+      </div>
+ 
+ 
+      <div className="Page-bottom">
+ 
+ 
+        <PlayBar />
+ 
+ 
+      </div>
+ 
+ 
+    </div>
+ 
+ 
+  );
+ 
+ 
+ };
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ const Header = () => {
+ 
+ 
+  return (
+ 
+ 
+    <div className="Page-header">
+ 
+ 
+      <h1>Reverb</h1>
+ 
+ 
+    </div>
+ 
+ 
+  );
+ 
+ 
+ };
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ const LeftBar = () => {
+ 
+ 
+  return (
+ 
+ 
+    <div className="Left-bar">
+ 
+ 
+      <button>Navigation</button>
+ 
+ 
+        <button>Search</button>
+ 
+ 
+    </div>
+ 
+ 
+  );
+ 
+ 
+ };
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ const Body = () => {
+ 
+ 
+  let user = "TempUser111";
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  return (
+ 
+ 
+    <div className="Center-content">
+ 
+ 
+      <h4>Good Morning, {user}</h4>
+ 
+ 
+        <div>Songs</div>
+ 
+ 
+        <div>Artists</div>
+ 
+ 
+        <div>Albums</div>
+ 
+ 
+        <div>Podcasts</div>
+ 
+ 
+        <div>Playlists</div>
+ 
+ 
+        <div>Genres</div>
+ 
+ 
+        {/* <GenreList /> */}
+ 
+ 
+    </div>
+ 
+ 
+  )
+ 
+ 
+ };
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ const RightBar = () => {
+ 
+ 
+ 
+ 
+ 
+ 
+  return (
+ 
+ 
+    <div className="Right-bar">
+ 
+ 
+      <button>Settings</button>
+ 
+ 
+    </div>
+ 
+ 
+  );
+ 
+ 
+ };
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ const PlayBar = () => {
+ 
+ 
+  return (
+ 
+ 
+    <div className="Play-bar">
+ 
+ 
+      <div className="Song-bar">THIS IS A SONG BAR</div>
+ 
+ 
+    </div>
+ 
+ 
+  )
+ 
+ 
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ // const GenreList = () => {
+ 
+ 
+ //   const mainGenreList = {
+ 
+ 
+ //     "Pop": ["artist1", "artist2"],
+ 
+ 
+ //     "Rock": [],
+ 
+ 
+ //     "Hip Hop": [],
+ 
+ 
+ //     "Country": [],
+ 
+ 
+ //     "Classical": [],
+ 
+ 
+ //     "Jazz": [],
+ 
+ 
+ //     "Electronic": [],
+ 
+ 
+ //     "Heavy Metal": [],
+ 
+ 
+ //   };
+ 
+ 
+ //   return (
+ 
+ 
+ //     <MainGenreList />
+ 
+ 
+ //   );
+ 
+ 
+ // }
+ 
+  
+ export default LibraryPage;
+ 
