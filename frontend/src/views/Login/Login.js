ac3d0e130b4b152e16791b6f2e866970c1c6254c
Nathan Pare <nathanpare@Nathans-MBP.lan>
2022-07-29 10:35:46 -0700

    Home Page

---
+++ frontend/src/views/Login/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <div className='body'>
+      <div class="login-button">
+    <button class="btn btn-primary btn-lg" type="submit">Login with Reverb</button>
+  </div>
+    </div>
+  )
+}
