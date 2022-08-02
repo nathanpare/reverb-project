ad7547e48f476f7edff5c0533a14784c81dfdeae
robinshrestha12 <robinshrestha.maple@gmail.com>
2022-08-02 14:11:47 -0400

    app.js re add

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import LibraryPage from './views/Library/library';
+//import {BrowserRouter as Router, Switch, Route} from 'react-dom';
+//import Footer from './components/footer/Footer';
+//import Sidebar from './views/sidebar/Sidebar'
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import Login from './views/Login/Login';
+import "./views/Login/Login.css"
+import Search from './views/Search/Search';
+
+const code = new URLSearchParams(window.location.search).get("code")
+
+function App() {
+  return (
+    <div className='app'>{code ? <Search code={code} /> : <Login />}</div>
+  );
+}
+
+
+export default App;
