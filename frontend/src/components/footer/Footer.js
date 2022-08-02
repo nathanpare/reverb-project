4bf5f56df1e08f71d0c665e24ac57bca90411e12
robinshrestha12 <robinshrestha.maple@gmail.com>
2022-08-02 12:31:05 -0400

    footer completed

--- frontend/src/components/footer/Footer.js
+++ frontend/src/components/footer/Footer.js
@@ -4,28 +4,36 @@ import { Box, Container, Row, Column, FooterLink, Heading } from "./FooterStyles
 
 function Footer() {
   return (
-  <Box>
-    <h1> Reverb - Your music library !!!</h1>
-    <Container>
-      <Row>
-        <Column>
-        <Heading>
-          About Us
-        </Heading>
-        </Column>
-        <Column>
-        <Heading>
-          Services
-        </Heading>
-        </Column>
-        <Column>
-        <Heading>
-          Contact Us
-        </Heading>
-        </Column>
-      </Row>
-    </Container>
-  </Box>
-)      }
+    <Box>     
+      <Container>
+        <Row>
+          <Column>
+            <Heading>
+              About Us
+            </Heading>
+            <FooterLink href='#'>Vision</FooterLink>
+          </Column>
+
+          <Column>
+            <Heading>
+              App Feature
+            </Heading>
+            <FooterLink href='#'>Playing Songs</FooterLink>
+            <FooterLink href='#'>Playlists</FooterLink>
+            <FooterLink href='#'>Liked Songs</FooterLink>
+          </Column>
+          <Column>
+            <Heading>
+              Contact Us
+            </Heading>
+            <FooterLink href='#'>Ottawa</FooterLink>
+            <FooterLink href='#'>Edminton</FooterLink>
+            <FooterLink href='#'>Vancouver</FooterLink>
+          </Column>
+        </Row>
+      </Container>
+    </Box>
+  )
+}
 
 export default Footer;
---
+++ frontend/src/components/footer/FooterStyles.js
@@ -0,0 +1,62 @@
+import styled from "styled-components";
+
+const Box = styled.div`
+padding: 0px 0px;
+background: darkblue;
+position: absolute;
+bottom: 0;
+width: 100%;
+@media (min-width: 1000px) {
+  height:20%;
+}
+`;
+
+const Container = styled.div`
+display: flex;
+flexdirection: column;
+justify-content:center;
+max-width: 100%;
+margin: 0 auto;
+`;
+
+const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+  margin-left: 20px;
+ 
+  @media (min-width: 1000px) {    
+   padding-left:120px;
+   padding-right: 120px;
+  }
+
+`;
+   
+const Row = styled.div`
+  display: flex;
+  vertical-align: top;
+  justify-content: center;
+`;
+   
+
+const FooterLink = styled.a`
+color: #fff;
+margin-bottom: 10px;
+font-size: 16px;
+font-family: fantasy, Helvetica, sans-serif;
+text-decoration: none;
+
+&:hover {
+  color: green;
+  transition:200ms ease-in;
+}
+`;
+const Heading = styled.p`
+  font-size: 22px;
+  color: #fff;
+  margin-bottom: 10px;
+  font-weight: bold;
+`;
+export { Box, Container, Column, Row, FooterLink, Heading};
+
+
