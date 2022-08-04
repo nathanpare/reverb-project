dc9b44079acecd5b19f82a3285773a2f4407bcf2
Nathan Pare <nathanpare@Nathans-MBP.lan>
2022-08-04 10:25:41 -0700

    Change authentication logic

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,61 +1,60 @@
-const pool = require('./connection');
-require("dotenv").config()
-const express = require("express")
-const cors = require("cors")
-const bodyParser = require("body-parser")
-const SpotifyWebApi = require("spotify-web-api-node")
+// const pool = require('./connection');
+// const express = require("express")
+// const cors = require("cors")
+// const bodyParser = require("body-parser")
+// const SpotifyWebApi = require("spotify-web-api-node")
 
-const app = express()
-app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+// const app = express()
+// app.use(cors())
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({ extended: true }))
 
-// app.listen(port, () => {
-//   console.log(`Music library server nunning on ${port}`);
-// });
-app.post("/refresh", (req, res) => {
-  const refreshToken = req.body.refreshToken
-  const spotifyApi = new SpotifyWebApi({
-    redirectUri: process.env.REDIRECT_URI,
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    refreshToken,
-  })
+// // app.listen(port, () => {
+// //   console.log(`Music library server nunning on ${port}`);
+// // });
+// app.post("/refresh", (req, res) => {
+//   const refreshToken = req.body.refreshToken
+//   const spotifyApi = new SpotifyWebApi({
+//     redirectUri: process.env.REDIRECT_URI,
+//     clientId: process.env.CLIENT_ID,
+//     clientSecret: process.env.CLIENT_SECRET,
+//     refreshToken,
+//   })
 
-  spotifyApi
-    .refreshAccessToken()
-    .then(data => {
-      res.json({
-        accessToken: data.body.accessToken,
-        expiresIn: data.body.expiresIn,
-      })
-    })
-    .catch(err => {
-      console.log(err)
-      res.sendStatus(400)
-    })
-})
+//   spotifyApi
+//     .refreshAccessToken()
+//     .then(data => {
+//       res.json({
+//         accessToken: data.body.accessToken,
+//         expiresIn: data.body.expiresIn,
+//       })
+//     })
+//     .catch(err => {
+//       console.log(err)
+//       res.sendStatus(400)
+//     })
+// })
 
-app.post("/login", (req, res) => {
-  const code = req.body.code
-  const spotifyApi = new SpotifyWebApi({
-    redirectUri: process.env.REDIRECT_URI,
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-  })
+// app.post("/login", (req, res) => {
+//   const code = req.body.code
+//   const spotifyApi = new SpotifyWebApi({
+//     redirectUri: process.env.REDIRECT_URI,
+//     clientId: process.env.CLIENT_ID,
+//     clientSecret: process.env.CLIENT_SECRET,
+//   })
 
-  spotifyApi
-    .authorizationCodeGrant(code)
-    .then(data => {
-      res.json({
-        accessToken: data.body.access_token,
-        refreshToken: data.body.refresh_token,
-        expiresIn: data.body.expires_in,
-      })
-    })
-    .catch(err => {
-      res.sendStatus(400)
-    })
-})
+//   spotifyApi
+//     .authorizationCodeGrant(code)
+//     .then(data => {
+//       res.json({
+//         accessToken: data.body.access_token,
+//         refreshToken: data.body.refresh_token,
+//         expiresIn: data.body.expires_in,
+//       })
+//     })
+//     .catch(err => {
+//       res.sendStatus(400)
+//     })
+// })
 
-app.listen(3002)
+// app.listen(3002)
--- frontend/src/views/Dashboard/Dashboard.js
+++ frontend/src/views/Dashboard/Dashboard.js
@@ -1,104 +1,104 @@
 import React from 'react'
 
-import SpotifyWebApi from 'spotify-web-api-node';
+// import SpotifyWebApi from 'spotify-web-api-node';
 
-const spotifyApi = new SpotifyWebApi({
-  clientId: "ea696b669de04879856700d07e3f9ac0",
-})
+// const spotifyApi = new SpotifyWebApi({
+//   clientId: "ea696b669de04879856700d07e3f9ac0",
+// })
 
 export default function Dashboard() {
   return (
     <div className='dashboard'>
       <div className="message">
-      <p>Welcome !</p>
-    </div>
+        <p>Welcome !</p>
+      </div>
 
-    <div className="featured">
-      <div className="title">
-        <p>Featured</p>
-      </div>
-      <div className="featured-songs">
-        <div className="song1">
-          <div className="image">
-            <a href="https://www.qries.com/">
-         <img alt={""} src="https://static.spin.com/files/2018/05/beerbongs-bentleys-cover-1525284635.jpg"
-         width="150" height="150" />
-      </a>
-          </div>
-          <p className="name">Over Now</p>
-          <p className="artist">Post Malone</p>
-          <i className="fa-solid fa-play"></i>
+      <div className="featured">
+        <div className="title">
+          <p>Featured</p>
         </div>
-        
-         <div className="song2">
-           <div className="image">
-            <a href="https://www.qries.com/">
-         <img alt={""} src="https://i.scdn.co/image/ab67616d0000b273626745b3aa04899001a924ad"
-         width="150" height="150" />
-      </a>
+        <div className="featured-songs">
+          <div className="song1">
+            <div className="image">
+              <a href="https://www.qries.com/">
+                <img alt={""} src="https://static.spin.com/files/2018/05/beerbongs-bentleys-cover-1525284635.jpg"
+                  width="150" height="150" />
+              </a>
+            </div>
+            <p className="name">Over Now</p>
+            <p className="artist">Post Malone</p>
+            <i className="fa-solid fa-play"></i>
           </div>
-          <p className="name">Low Life</p>
-          <p className="artist">The Weekend</p>
-           <i className="fa-solid fa-play"></i>
-        </div>
-        
-         <div className="song3">
-           <div className="image">
-            <a href="https://www.qries.com/">
-         <img alt={""} src="https://i.pinimg.com/originals/05/d9/72/05d9729fb2eb1e891b9ee56ed315e298.jpg"
-         width="150" height="150" />
-      </a>
+
+          <div className="song2">
+            <div className="image">
+              <a href="https://www.qries.com/">
+                <img alt={""} src="https://i.scdn.co/image/ab67616d0000b273626745b3aa04899001a924ad"
+                  width="150" height="150" />
+              </a>
+            </div>
+            <p className="name">Low Life</p>
+            <p className="artist">The Weekend</p>
+            <i className="fa-solid fa-play"></i>
           </div>
-          <p className="name">YAYA</p>
-          <p className="artist">6ix9ine</p>
-           <i className="fa-solid fa-play"></i>
-        </div>
-      </div>
-    </div>
-    
-    <div className="featured">
-      <div className="title">
-        <p>Albums of the summer
-</p>
-      </div>
-      <div className="featured-songs">
-        <div className="song1">
-          <div className="image">
-            <a href="https://www.qries.com/">
-         <img alt={""} src="https://wallpapercave.com/wp/wp4685864.jpg"
-         width="150" height="150" />
-      </a>
+
+          <div className="song3">
+            <div className="image">
+              <a href="https://www.qries.com/">
+                <img alt={""} src="https://i.pinimg.com/originals/05/d9/72/05d9729fb2eb1e891b9ee56ed315e298.jpg"
+                  width="150" height="150" />
+              </a>
+            </div>
+            <p className="name">YAYA</p>
+            <p className="artist">6ix9ine</p>
+            <i className="fa-solid fa-play"></i>
           </div>
-          <p className="name">Dark Knight Dummo</p>
-          <p className="artist">Trippi Redd</p>
-          <i className="fa-solid fa-play"></i>
-        </div>
-        
-         <div className="song2">
-           <div className="image">
-            <a href="https://www.qries.com/">
-         <img alt={""} src="https://external-preview.redd.it/B3CA06azeiKnZ2UlDYQmNjEIjeK9EkvX1U-plGW4WrM.jpg?auto=webp&s=fc62536311988c7800558274a220a3a177842fc4"
-         width="150" height="150" />
-      </a>
-          </div>
-          <p className="name">All Time Low</p>
-          <p className="artist">Jon Bellion</p>
-           <i className="fa-solid fa-play"></i>
-        </div>
-        
-         <div className="song3">
-           <div className="image">
-            <a href="https://www.qries.com/">
-         <img alt={""} src="https://i.scdn.co/image/ab67616d0000b273ce159a3ba2096e13fa9d4b4c"
-         width="150" height="150" />
-      </a>
-          </div>
-          <p className="name">Never Recover</p>
-          <p className="artist">Lil Baby, Drake, Gunna</p>
-           <i className="fa-solid fa-play"></i>
         </div>
       </div>
-    </div>
+
+      <div className="featured">
+        <div className="title">
+          <p>Albums of the summer
+          </p>
+        </div>
+        <div className="featured-songs">
+          <div className="song1">
+            <div className="image">
+              <a href="https://www.qries.com/">
+                <img alt={""} src="https://wallpapercave.com/wp/wp4685864.jpg"
+                  width="150" height="150" />
+              </a>
+            </div>
+            <p className="name">Dark Knight Dummo</p>
+            <p className="artist">Trippi Redd</p>
+            <i className="fa-solid fa-play"></i>
+          </div>
+
+          <div className="song2">
+            <div className="image">
+              <a href="https://www.qries.com/">
+                <img alt={""} src="https://external-preview.redd.it/B3CA06azeiKnZ2UlDYQmNjEIjeK9EkvX1U-plGW4WrM.jpg?auto=webp&s=fc62536311988c7800558274a220a3a177842fc4"
+                  width="150" height="150" />
+              </a>
+            </div>
+            <p className="name">All Time Low</p>
+            <p className="artist">Jon Bellion</p>
+            <i className="fa-solid fa-play"></i>
+          </div>
+
+          <div className="song3">
+            <div className="image">
+              <a href="https://www.qries.com/">
+                <img alt={""} src="https://i.scdn.co/image/ab67616d0000b273ce159a3ba2096e13fa9d4b4c"
+                  width="150" height="150" />
+              </a>
+            </div>
+            <p className="name">Never Recover</p>
+            <p className="artist">Lil Baby, Drake, Gunna</p>
+            <i className="fa-solid fa-play"></i>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
