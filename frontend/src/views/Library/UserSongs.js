4035d0dbcfeb2d0c8f93c068af2caefdb87ad0aa
Robert812218 <robert812@shaw.ca>
2022-08-17 09:01:46 -0600

    add to fix merge issue

---
+++ frontend/src/views/Library/UserSongs.js
@@ -0,0 +1,18 @@
+import React, {useEffect} from 'react';
+//import axios from 'axios';
+
+export default function UserSongs({title, artist}) {
+console.log(title);
+console.log(artist);
+  return (
+    <div className="song" >
+      <div className="image">
+        <img alt={""} src="https://www.pngegg.com/en/png-fnqfx"
+          width="150" height="150" />
+      </div>
+      <p className="name">{title}</p>
+      <p className="artist">{artist}</p>
+
+    </div>
+  )
+}
