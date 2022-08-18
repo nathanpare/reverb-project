ffc5228dc3b89e33a5575ea472c485d59901d685
robinshrestha12 <robinshrestha.maple@gmail.com>
2022-08-18 09:19:57 -0400

    got library image

--- frontend/src/views/Library/library.js
+++ frontend/src/views/Library/library.js
@@ -31,106 +31,106 @@ export default function Library(props) {
   const [userAlbums, setUserAlbums] = useState([]);
   const [userPlaylists, setUserPlaylists] = useState([]);
   const [userGenres, setUserGenres] = useState([]);
-  const [isFirstRender, setIsFirstRender]=useState("A");
+  const [isFirstRender, setIsFirstRender] = useState("A");
   const [userPlaylistId, setUserPlaylistId] = useState([]);
-  const [addRenderhtml, setAddRenderhtml]=useState("");
+  const [addRenderhtml, setAddRenderhtml] = useState("");
 
-  
+
   const [{ user, featured, summers, recents, tops, token }, dispatch] = UseDataLayerValue();
-  
 
- // console.log(userPlaylists);
- 
+
+ console.log(featured.tracks);
+
   const name = user.display_name;
   let addRender = "";
 
   useEffect(() => {
-   if(userPlaylists){
-    fetchData();
-    //setIsOneRender(False);
-   }
-    
-    
-    if(isFirstRender === "A"){
-      
+    if (userPlaylists) {
+      fetchData();
+      //setIsOneRender(False);
+    }
+
+
+    if (isFirstRender === "A") {
+
       const userId = user.id;
       // console.log(user);
-      
+
       axios.get(`http://localhost:8080/playlistsongs/${user.id}`)
-        .then(function(res) {
+        .then(function (res) {
           setUserPlaylists([...res.data.rows])
           // let info = res.data;
-          
-      
-          console.log(res.data.rows)
-          
+
+
+        //  console.log(res.data.rows)
+
         });
-        setIsFirstRender("B");
+      setIsFirstRender("B");
     }
-   
-        
-      
+
+
+
   }, []);
 
-  
-   
-  
-  function fetchUserSongs(playlistid){
-    console.log(allSongs);
-    console.log(typeof(playlistid));
-   // console.log(userId);
-    
+
+
+
+  function fetchUserSongs(playlistid) {
+   // console.log(allSongs);
+   // console.log(typeof (playlistid));
+    // console.log(userId);
+
     if (allSongs.length > 0) {
-      
-     // console.log(allSongs);
+
+      // console.log(allSongs);
       const filtered_userPlaylistSongs_table = allSongs.filter(playlistSong => {
         //console.log(user.spotify_name);
-        console.log(playlistSong.playlist_id);
-        return ( playlistSong.playlist_id === playlistid);
+       // console.log(playlistSong.playlist_id);
+        return (playlistSong.playlist_id === playlistid);
       })
-      console.log(filtered_userPlaylistSongs_table);
+    //  console.log(filtered_userPlaylistSongs_table);
       if (filtered_userPlaylistSongs_table.length === 0) {
         return;
-      } 
+      }
 
-    setUserPlaylistSongs(filtered_userPlaylistSongs_table);
+      setUserPlaylistSongs(filtered_userPlaylistSongs_table);
     }
-    console.log(userPlaylistSongs);
+   // console.log(userPlaylistSongs);
   }
 
- 
+
 
   const fetchData = () => {
-   
-  
+
+
     const songsApi = `http://localhost:8080/playlistsongs`;
 
     const getSongs = axios.get(songsApi);
-  
-  
+
+
     axios.all([getSongs]).then(
       axios.spread((...allData) => {
 
-        const allSongsData = allData[0].data;      
-    
-        console.log("all songs Data", allSongsData);
+        const allSongsData = allData[0].data;
+
+     //   console.log("all songs Data", allSongsData);
 
         setAllSongs([...allSongsData]);
-       
+
       })
     ).catch(error => {
       console.log(error);
     })
   };
-  function getSongs(playlist_id){
+  function getSongs(playlist_id) {
 
-    console.log(playlist_id);
+  //  console.log(playlist_id);
     fetchUserSongs(playlist_id);
     setUserPlaylistId(playlist_id);
-    console.log(userPlaylistSongs);
-       setAddRenderhtml(<UserSongs userPlaylistSongs={userPlaylistSongs}/>);
+   // console.log(userPlaylistSongs);
+    setAddRenderhtml(<UserSongs userPlaylistSongs={userPlaylistSongs} />);
   }
- 
+
   return (
 
     <div className="library-page">
@@ -138,70 +138,70 @@ export default function Library(props) {
         <h1 className="page-title">Reverb Library</h1>
         <h3 className="header-message">Good morning</h3>
       </header>
-        <div className="page-components">
-          <div className="nav-bar">
+      <div className="page-components">
+        <div className="nav-bar">
 
-          </div>
-          <div className="left-side">
-
-            <div className="songs-components">
-              <h5 className="songs-header">Songs</h5>
-                <div className="songs-items">
-                  {/* <SongsPreview /> */}
-                  <Button className="all-songs-button">All Songs</Button>
-                </div>
-            </div>
-            <div className="albums-components">
-              <h5 className="albums-header">Albums</h5>
-                <div className="albums-items">
-                  {/* <AlbumsPreview /> */}
-                  <Button className="all-albums-button">All Albums</Button>
-                </div>
-            </div>
-            <div className="artists-components">
-              <h5 className="artists-header">Artists</h5>
-                <div className="artists-items">
-                  {/* <ArtistsPreview /> */}
-                  <Button className="all-artists-button">All Artists</Button>
-                </div>
-            </div>
-          </div>
-
-          <div className="right-side">
-
-            <div className="playlists-components">
-              <h5 className="playlists-header">Playlists</h5>
-                <div className="playlists-items">
-                  {/* < userPlaylists={userPlaylists} />  */}
-                  {userPlaylists.map((playlist, index) => {
-                    return (
-                      <div key={index}>
-                        <Button className="all-playlists-button" onClick={() => getSongs(playlist.id)}>{playlist.name}</Button>
-                      </div>
-                    )
-                    })}
-                  {/* <Button className="all-playlists-button" onClick={() => {}}>All Playlists</Button> */}
-                </div>
-            </div>
-            <div className="genres-components">
-              <h5 className="genres-header">Genres</h5>
-                <div className="genres-items">
-                  {/* <GenresPreview /> */}
-                  <Button className="all-genres-button">All Genres</Button>
-                </div>
-            </div>
-            <div className="podcasts-components">
-              <h5 className="podcasts-header">Podcasts</h5>
-                <div className="podcasts-items">
-                  {/* <PodcastsPreview /> */}
-                  <Button className="all-podcasts-button">All Podcasts</Button>
-                </div>
-            </div>
-
-          </div>         
         </div>
-        
-      
+        <div className="left-side">
+
+          <div className="songs-components">
+            <h5 className="songs-header">Songs</h5>
+            <div className="songs-items">
+              {/* <SongsPreview /> */}
+              <Button className="all-songs-button">All Songs</Button>
+            </div>
+          </div>
+          <div className="albums-components">
+            <h5 className="albums-header">Albums</h5>
+            <div className="albums-items">
+              {/* <AlbumsPreview /> */}
+              <Button className="all-albums-button">All Albums</Button>
+            </div>
+          </div>
+          <div className="artists-components">
+            <h5 className="artists-header">Artists</h5>
+            <div className="artists-items">
+              {/* <ArtistsPreview /> */}
+              <Button className="all-artists-button">All Artists</Button>
+            </div>
+          </div>
+        </div>
+
+        <div className="right-side">
+
+          <div className="playlists-components">
+            <h5 className="playlists-header">Playlists</h5>
+            <div className="playlists-items">
+              {/* < userPlaylists={userPlaylists} />  */}
+              {userPlaylists.map((playlist, index) => {
+                return (
+                  <div key={index}>
+                    <Button className="all-playlists-button" onClick={() => getSongs(playlist.id)}>{playlist.name}</Button>
+                  </div>
+                )
+              })}
+              {/* <Button className="all-playlists-button" onClick={() => {}}>All Playlists</Button> */}
+            </div>
+          </div>
+          <div className="genres-components">
+            <h5 className="genres-header">Genres</h5>
+            <div className="genres-items">
+              {/* <GenresPreview /> */}
+              <Button className="all-genres-button">All Genres</Button>
+            </div>
+          </div>
+          <div className="podcasts-components">
+            <h5 className="podcasts-header">Podcasts</h5>
+            <div className="podcasts-items">
+              {/* <PodcastsPreview /> */}
+              <Button className="all-podcasts-button">All Podcasts</Button>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+
       {/* {addRender}
       {userPlaylistSongs.map((playlist, index) => {
                     return (
@@ -210,21 +210,31 @@ export default function Library(props) {
                       </div>
                     )
                     })} */}
-          <div className="featured-songs">
-         { userPlaylistSongs.map((playlist, index) => {
- return (
-  <div key={index}>
- <UserSongs title={playlist.title} artist={playlist.artist} img={playlist.img} />
+      <div className="featured-songs">
+        {userPlaylistSongs.map((playlist, index) => {
+          return (
+            <div key={index}>
+              <UserSongs title={playlist.title} artist={playlist.artist} img={playlist.img} />
+            </div>
+          )
 
-
-  </div>
- )
- 
-         }
-         )
-          }
-        </div> 
-        <Footer />
+        }
+        )
+        }
+      </div>
+      <div className="releases-songs">
+          {/* {releases?.albums?.items.map((item) => (
+            <UserSongs
+              title={item.name}
+              artist={item.artists[0].name}
+              img={item.images[0].url}
+             // setPlayingTrack={setPlayingTrack}
+             // item={item}
+             // onPlay={setPlayingTrack} 
+              />
+          ))} */}
+        </div>
+      <Footer />
     </div>
   );
 }
